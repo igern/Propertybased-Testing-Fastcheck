45c28e3e3bcf71c35afadc170d9e65ddb61376e4
jonas <jones16511@hotmail.com>
2019-05-04 10:55:03 +0200

    Update ourObject.ts

--- src/ourObject.ts
+++ src/ourObject.ts
@@ -1,11 +1,14 @@
-import fc, { Arbitrary } from 'fast-check';
+import fc, { Arbitrary, Random, Shrinkable } from 'fast-check';
+
 
 
 export const betterObject: Arbitrary<Object> = fc.object().filter((o) => {
     let returning = true
+    if (Object.keys(o).length == 0) {
+        returning = Math.random() > 0.5;
+    }
     Object.keys(o).forEach((key) => {
-
-        if (o[key] instanceof Array || o[key] instanceof Object) {
+        if (o[key] instanceof Object) {
             returning = false;
         }
     })
@@ -14,7 +17,9 @@ export const betterObject: Arbitrary<Object> = fc.object().filter((o) => {
 
 // fc.statistics(
 //     betterObject,
-//     v => Object.keys(v).length < 5 ? 'Small dictionary' : 'Big dictionary',
-//     { numRuns: 10000, logger: console.log }
+//     v => Object.keys(v).length.toString(),
+//     { numRuns: 100, logger: console.log }
 // );
+
+//console.log(fc.sample(betterObject, 10))
 
